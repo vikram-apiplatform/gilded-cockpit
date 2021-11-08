d9ebd7c8b2ee13d6ececbea6f735f2f5f4e3bfbc
Shanthan <shanthan.konka@apiplatform.io>
2021-11-08 10:06:13 +0530

    Added bootstrap

---
+++ src/app/dashbord-overview/dashbord-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashbord-overview',
+  templateUrl: './dashbord-overview.component.html',
+  styleUrls: ['./dashbord-overview.component.css']
+})
+export class DashbordOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
